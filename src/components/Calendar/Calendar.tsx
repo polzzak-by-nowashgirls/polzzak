b3ef03256e0383c3c40777960cd18262f177d60b
ttining <ttining.real@gmail.com>
2025-03-21 17:37:37 +0900

    [Modify] 달력 컴포넌트 스타일 수정정

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -16,32 +16,30 @@ function Calendar({
       showOutsideDays={showOutsideDays}
       className={cn('border-none p-2', className)}
       classNames={{
-        months: 'flex flex-col sm:flex-row gap-2',
-        month: 'w-[280px] m-auto flex flex-col gap-3',
-        caption: 'flex justify-center pt-1 relative items-center w-full',
-        caption_label: 'text-sm font-medium',
+        months: 'max-w-2xs m-auto',
+        month: 'p-2 flex flex-col gap-4',
+        caption: 'flex justify-center relative items-center w-full',
+        caption_label: 'text-16 font-semibold',
         nav: 'flex items-center gap-1',
         nav_button: cn(
           ButtonVariants({ variant: 'tertiary' }),
-          'size-7 p-0 text-black cursor-pointer hover:text-primary-hover active:text-primary',
+          'text-center size-7 p-0 text-black cursor-pointer hover:text-primary-hover active:text-primary',
         ),
-        nav_button_previous: 'absolute left-1',
-        nav_button_next: 'absolute right-1',
-        table: 'w-full border-collapse space-x-1',
+        nav_button_previous: 'absolute left-1 flex items-center justify-center',
+        nav_button_next: 'absolute right-1 flex items-center justify-center',
+        table: 'flex flex-col gap-2 w-full border-collapse space-x-1',
         head_row: 'flex',
-        head_cell:
-          'flex-1 bg-gray-20 text-black rounded-md font-medium text-14',
-        row: 'flex w-full',
+        head_cell: 'flex-1 py-1 font-medium text-14 text-black',
+        row: 'flex my-1',
         cell: cn(
-          // 'w-10 relative p-1 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-primary/20',
-          'flex-1 relative p-1 text-center text-sm focus-within:relative focus-within:z-20',
+          'flex flex-1 items-center justify-center p-1 relative font-medium text-14 focus-within:relative focus-within:z-20',
           props.mode === 'range'
             ? '[&:has([aria-selected])]:bg-primary/20 [&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md [&:has(.day-today[aria-selected])]:rounded-md'
             : '[&:has([aria-selected])]:rounded-md',
         ),
         day: cn(
           ButtonVariants({ variant: 'tertiary' }),
-          'size-8 p-0 font-medium rounded-sm aria-selected:opacity-100 hover:bg-primary-hover hover:text-white cursor-pointer',
+          'aspect-square flex-1 text-14 font-medium rounded-sm aria-selected:opacity-100 hover:bg-primary-hover hover:text-white cursor-pointer',
         ),
         day_range_start:
           'day-range-start aria-selected:bg-primary aria-selected:text-white',
