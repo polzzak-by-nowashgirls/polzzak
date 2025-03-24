a2b80af8d73fe5fa3f0019610a2983917f080fc3
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-24 18:23:41 +0900

    [Modify] Icon 컴포넌트 수정
    - 컴포넌트명 변경 (SvgIcon > Icon)
    - 함수타입 변경
    
    유형: #41

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,15 +1,15 @@
-interface SvgIconProps {
+interface IconProps {
   id: string;
   size?: number;
   className?: string;
 }
 
-const SvgIcon = ({ id, size = 24, className = '' }: SvgIconProps) => {
+function Icon({ id, size = 24, className = '' }: IconProps) {
   return (
     <svg width={size} height={size} className={className}>
       <use xlinkHref={`/stack.svg#${id}`} />
     </svg>
   );
-};
+}
 
-export default SvgIcon;
+export default Icon;
