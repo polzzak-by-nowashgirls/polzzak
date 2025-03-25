d1824ccc02baa32baaa5265254152e63682837a6
kimHyeonJu <kimhyesom1202@gmail.com>
2025-03-25 16:56:33 +0900

    [Feature] 리스트 아이템 컴포넌트 제작
    - 이미지
    - 장소명
    - 주소
    - 즐겨찾기, 리뷰 개수 카운트
    - 즐겨찾기 버튼
    - 클릭이벤트
    
    #36

---
+++ src/components/ListItem/ListItem.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+
+import Button from '@/components/Button/Button';
+import SvgIcon from '@/components/Icon/Icon';
+import { LIST_ITEM_DUMMY_DATA } from '@/mockData/ListItemDummyData';
+
+function ListItem() {
+  const ListItemData = LIST_ITEM_DUMMY_DATA;
+  const [isCheck, setIsCheck] = useState(ListItemData.map(() => false));
+  const [favoriteCount, setFavoriteCount] = useState(
+    ListItemData.map((count) => count.favorite_count),
+  );
+  const handleFavorite = (id: number) => {
+    setIsCheck((prev) =>
+      prev.map((state, index) =>
+        ListItemData[index].id === id ? !state : state,
+      ),
+    );
+
+    setFavoriteCount((prev) =>
+      prev.map((count, index) =>
+        index === id ? (isCheck[id] ? count - 1 : count + 1) : count,
+      ),
+    );
+  };
+
+  return (
+    <ul className="flex flex-col gap-6">
+      {ListItemData.map((item) => (
+        <li
+          className="flex-start relative flex w-full items-center gap-4"
+          key={item.id}
+        >
+          <div className="flex h-20 w-20 shrink-0 items-center justify-center overflow-hidden rounded-2xl object-cover">
+            <img src={item.imgUrl} alt={item.title} className="h-full w-full" />
+          </div>
+          <div className="flex w-0 flex-1 flex-col">
+            <div className="flex items-center justify-between">
+              <h3
+                className="fs-14 ls lh w-full truncate font-semibold text-black"
+                aria-label={item.title}
+              >
+                {item.title}
+              </h3>
+              <Button
+                variant="tertiary"
+                size="md"
+                className='text-primary [&_svg:not([class*="size-"])]:size-5'
+                onClick={() => handleFavorite(item.id)}
+                aria-label={isCheck ? '즐겨찾기 취소' : '즐겨찾기 추가'}
+                aria-live="polite"
+              >
+                <SvgIcon
+                  id={isCheck[item.id] ? 'favorite-true' : 'favorite-false'}
+                  className="text-primary"
+                />
+              </Button>
+            </div>
+            <span className="fs-14 ls lh font-regular text-gray07">
+              {item.start_date} &#126; {item.end_date}
+            </span>
+            <span className="fs-14 ls lh font-regular text-gray07 inline-flex items-center">
+              {item.region}
+              <SvgIcon
+                id="arrow-small-right"
+                size={16}
+                className="text-gray07"
+              />
+              {item.district}
+            </span>
+            <dl className="fs-14 ls lh font-regular text-gray07 flex gap-2">
+              <div className="flex items-center gap-1" aria-label="즐겨찾기 수">
+                <dt>
+                  <SvgIcon id="favorite-false" size={16} />
+                </dt>
+                <dd>
+                  {favoriteCount[item.id] >= 999
+                    ? '+999'
+                    : favoriteCount[item.id]}
+                </dd>
+              </div>
+              <div className="flex items-center gap-1" aria-label="리뷰 수">
+                <dt>
+                  <SvgIcon id="review" size={16} />
+                </dt>
+                <dd>{item.review_count}</dd>
+              </div>
+            </dl>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListItem;
---
+++ src/mockData/ListItemDummyData.tsx
@@ -0,0 +1,62 @@
+export const LIST_ITEM_DUMMY_DATA = [
+  {
+    id: 0,
+    imgUrl:
+      'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    title: '2025 보름왓 튤립 축제2025 보름왓 튤립 축제2025 보름왓 튤립 축제',
+    start_date: '2025.03.22',
+    end_date: '2025.04.13',
+    region: '지역', // areaNm : 관광지 지역명
+    district: '시군구 주소', // signguNm : 관광지 시군구명
+    favorite_count: 1005,
+    review_count: 88,
+  },
+  {
+    id: 1,
+    imgUrl:
+      'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    title: '2025 보름왓 튤립 축제2025 보름왓 튤립 축제2025 보름왓 튤립 축제',
+    start_date: '2025.03.22',
+    end_date: '2025.04.13',
+    region: '지역',
+    district: '시군구 주소',
+    favorite_count: 107,
+    review_count: 88,
+  },
+  {
+    id: 2,
+    imgUrl:
+      'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    title: '2025 보름왓 튤립 축제',
+    start_date: '2025.03.22',
+    end_date: '2025.04.13',
+    region: '지역',
+    district: '시군구 주소',
+    favorite_count: 10,
+    review_count: 88,
+  },
+  {
+    id: 3,
+    imgUrl:
+      'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    title: '2025 보름왓 튤립 축제',
+    start_date: '2025.03.22',
+    end_date: '2025.04.13',
+    region: '지역',
+    district: '시군구 주소',
+    favorite_count: 998,
+    review_count: 88,
+  },
+  {
+    id: 4,
+    imgUrl:
+      'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    title: '2025 보름왓 튤립 축제',
+    start_date: '2025.03.22',
+    end_date: '2025.04.13',
+    region: '지역',
+    district: '시군구 주소',
+    favorite_count: 999,
+    review_count: 88,
+  },
+];
