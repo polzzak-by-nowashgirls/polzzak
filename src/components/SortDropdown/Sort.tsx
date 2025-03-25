b5f019ab9ba158d9c3354028de113eb8c79a983c
ttining <ttining.real@gmail.com>
2025-03-25 15:14:25 +0900

    [Modify] 정렬 드롭다운 버튼 컴포넌트
    - 스크롤 useRef 및 드래그 관련 추가
    - globals.css 수정

--- src/components/SortDropdown/Sort.tsx
+++ src/components/SortDropdown/Sort.tsx
@@ -1,11 +1,52 @@
+import { useRef, useState } from 'react';
+
 import Dropdown from '@/components/SortDropdown/Dropdown';
 import { dropdownData } from '@/mockData/SortDropdownData';
 
 function Sort() {
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+  const [isDown, setIsDown] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  const startDragging = (
+    e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>,
+  ) => {
+    if (!scrollRef.current) return;
+    setIsDown(true);
+    const pageX = 'touches' in e ? e.touches[0].pageX : e.pageX;
+    setStartX(pageX);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  };
+
+  const stopDragging = () => {
+    setIsDown(false);
+  };
+
+  const move = (
+    e: React.MouseEvent<HTMLDivElement> | React.TouchEvent<HTMLDivElement>,
+  ) => {
+    if (!isDown || !scrollRef.current) return;
+    e.preventDefault();
+    const pageX = 'touches' in e ? e.touches[0].pageX : e.pageX;
+    const walk = (pageX - startX) * 1.5; // 드래그 속도 조절
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  };
+
   return (
-    <div className="sort-scroll flex overflow-x-auto p-1">
+    <div
+      ref={scrollRef}
+      onMouseDown={startDragging}
+      onMouseLeave={stopDragging}
+      onMouseUp={stopDragging}
+      onMouseMove={move}
+      onTouchStart={startDragging}
+      onTouchEnd={stopDragging}
+      onTouchMove={move}
+      className="sort-scroll flex flex-nowrap overflow-x-auto p-1"
+    >
       {dropdownData.map((data) => (
-        <Dropdown label={data.label} options={data.list} />
+        <Dropdown key={data.label} label={data.label} options={data.list} />
       ))}
     </div>
   );
