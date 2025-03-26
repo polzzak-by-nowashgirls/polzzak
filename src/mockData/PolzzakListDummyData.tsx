63af71a645eac7e924373a839ffcb12ce91d3bb2
ttining <ttining.real@gmail.com>
2025-03-26 17:43:23 +0900

    [Feature] 폴짝 목록 컴포넌트 제작
    - 리스트 아이템
    - 더미데이터 생성

---
+++ src/mockData/PolzzakListDummyData.tsx
@@ -0,0 +1,60 @@
+export interface ListItemType {
+  label: string;
+  startDate?: string;
+  endDate?: string;
+  city: string[];
+}
+
+export interface PolzzakType {
+  title: string;
+  img: string;
+  list: ListItemType[];
+}
+
+export const POLZZAK_LIST: PolzzakType[] = [
+  {
+    title: '폴짝 중',
+    img: 'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    list: [
+      {
+        label: '인천에서 술 먹기',
+        startDate: '2025.03.15',
+        endDate: '2025.03.17',
+        city: ['인천'],
+      },
+    ],
+  },
+  {
+    title: '폴짝 준비',
+    img: 'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    list: [
+      {
+        label: '시골쥐 상경하다',
+        startDate: '2025.04.22',
+        endDate: '2025.04.23',
+        city: ['인천', '서울'],
+      },
+      {
+        label: '떠나요 셋이서',
+        city: ['제주'],
+      },
+    ],
+  },
+  {
+    title: '폴짝 완료',
+    img: 'https://tong.visitkorea.or.kr/cms/resource/55/3480455_image2_1.jpg',
+    list: [
+      {
+        label: '부산 갈매기',
+        startDate: '2025.01.05',
+        endDate: '2025.01.07',
+        city: ['부산'],
+      },
+      {
+        label: '보노보노의 기묘한 모험',
+        startDate: '2025.01.05',
+        city: ['제주', '여수', '전주'],
+      },
+    ],
+  },
+];
