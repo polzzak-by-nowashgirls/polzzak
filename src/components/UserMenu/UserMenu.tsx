7b2add5c57d88a64c2fbab29f49e7fa3c8008ec9
ttining <ttining.real@gmail.com>
2025-03-27 17:57:05 +0900

    [Feature] 유저 메뉴 컴포넌트
    - 유저 메뉴 컴포넌트 제작
    - 참고용으로 My 페이지에 컴포넌트 추가해두었습니다 !

---
+++ src/components/UserMenu/UserMenu.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router-dom';
+
+import Icon from '@/components/Icon/Icon';
+
+interface MenuItem {
+  label: string;
+  icon: string;
+  path?: string;
+  onClick?: () => void;
+}
+
+interface UserMenuProps {
+  menus: MenuItem[];
+}
+
+function UserMenu({ menus }: UserMenuProps) {
+  return (
+    <div className="m-auto flex max-w-md gap-4 px-1 py-0.5">
+      {menus.map((item, index) =>
+        item.onClick ? (
+          <button
+            key={index}
+            onClick={item.onClick}
+            className="fs-14 font-regular focus-visible:border-ring focus-visible:ring-ring flex flex-1 flex-col items-center gap-2 rounded-sm py-3 outline-none focus-visible:ring-[2px] focus-visible:ring-offset-2"
+          >
+            <Icon id={item.icon} />
+            {item.label}
+          </button>
+        ) : (
+          <Link
+            to={item.path as string}
+            key={index}
+            className="fs-14 font-regular focus-visible:border-ring focus-visible:ring-ring flex flex-1 flex-col items-center gap-2 rounded-sm py-3 outline-none focus-visible:ring-[2px] focus-visible:ring-offset-2"
+          >
+            <Icon id={item.icon} />
+            {item.label}
+          </Link>
+        ),
+      )}
+    </div>
+  );
+}
+
+export default UserMenu;
