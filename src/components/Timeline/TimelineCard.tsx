ffcb81309e7597cf70e83035e22a9f0ad473b801
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-30 23:03:07 +0900

    [Feature] 타임라인 컴포넌트 제작(추가)
    - 왼쪽으로 드래그 움직임 있을 시, 편집 버튼 생성
    - 사용 예시: `<TimelineList />`
    
    유형: #81

--- src/components/Timeline/TimelineCard.tsx
+++ src/components/Timeline/TimelineCard.tsx
@@ -1,17 +1,59 @@
-// import Button from '@/components/Button/Button';
+import {
+  animate,
+  motion,
+  Reorder,
+  useDragControls,
+  useMotionValue,
+  useTransform,
+} from 'framer-motion';
+import { useEffect } from 'react';
 
-import { Reorder, useDragControls } from 'framer-motion';
-
+import Button from '@/components/Button/Button';
 import Icon from '@/components/Icon/Icon';
 import { cn } from '@/lib/utils';
 import { PolzzakCard } from '@/mockData/PolzzakItemDummyData';
 
 interface TimelineCard {
   value: PolzzakCard;
+  openCardId: number | null;
+  setOpenCardId: (id: number | null) => void;
 }
 
-function TimelineCard({ value }: TimelineCard) {
+function TimelineCard({ value, openCardId, setOpenCardId }: TimelineCard) {
+  const { id, place, time, memo } = value;
+
+  const isOpen = openCardId === id;
   const controls = useDragControls();
+  const x = useMotionValue(0);
+
+  const dragOpacity = useTransform(x, [-55, -10], [0, 1]);
+  const scale = useTransform(x, [-55, -10], [0.8, 1]);
+  const btnOpacity = useTransform(x, [-55, 0], [1, 0]);
+  const btnTranslate = useTransform(x, [-55, 0], [0, 100]);
+
+  useEffect(() => {
+    if (!isOpen) animate(x, 0);
+  }, [x, isOpen, controls]);
+
+  const handleDragEnd = () => {
+    if (x.get() > -40) {
+      animate(x, 0);
+      setOpenCardId(null);
+    } else {
+      animate(x, -55);
+      setOpenCardId(id);
+    }
+  };
+
+  const onClickOpenEditModal = () => {
+    console.log('‼️ Open Modal ‼️');
+  };
+
+  const handleEditButton = () => {
+    animate(x, 0);
+    setOpenCardId(null);
+    onClickOpenEditModal();
+  };
 
   return (
     <Reorder.Item
@@ -24,37 +66,57 @@ function TimelineCard({ value }: TimelineCard) {
         <div className="bg-primary absolute top-[26px] z-50 h-[11px] w-[11px] -translate-y-1/2 rounded-full"></div>
       </div>
 
-      <article className="min-w-0 flex-1 bg-white">
+      <article className="relative min-w-0 flex-1 bg-white">
+        <motion.div
+          drag={'x'}
+          dragConstraints={{ left: -55, right: 0 }}
+          style={{ x }}
+          onDragEnd={handleDragEnd}
+          className="absolute top-0 left-0 z-10 h-full w-[calc(100%-48px)]"
+        ></motion.div>
+
         <header
           className={cn(
             'border-gray03 flex items-center items-stretch justify-between rounded-md border py-3 pl-4',
           )}
         >
           <div className="min-w-0 flex-1">
-            <h4 className="fs-14 inline-block font-semibold">{value.place}</h4>
-            {value.time && (
-              <time className="fs-14 text-gray08 ml-1" dateTime={value.time}>
-                {value.time}
+            <h4 className="fs-14 inline-block font-semibold">{place}</h4>
+            {time && (
+              <time className="fs-14 text-gray08 ml-1" dateTime={time}>
+                {time}
               </time>
             )}
-            {value.memo && (
+            {memo && (
               <p className="fs-14 text-gray07 w-full truncate overflow-hidden text-ellipsis whitespace-nowrap">
-                {value.memo}
+                {memo}
               </p>
             )}
           </div>
-          <div
-            className="flex flex-shrink-0 items-center bg-red-100 px-4 select-none hover:bg-red-500"
+          <motion.div
+            style={{ opacity: dragOpacity, scale }}
+            className="flex flex-shrink-0 items-center px-4 select-none"
             onPointerDown={(e) => {
-              console.log(controls.start(e));
+              controls.start(e);
+              setOpenCardId(id);
             }}
           >
             <Icon id={'drag_handle'} className="text-gray05" size={16} />
-          </div>
+          </motion.div>
         </header>
-        {/* <footer>
-            <Button>편집</Button>
-          </footer> */}
+
+        <motion.footer
+          style={{ x: btnTranslate, opacity: btnOpacity }}
+          className="absolute top-0 right-0 flex h-full items-center"
+        >
+          <Button
+            size={'md'}
+            className="m-0 h-full w-[50px] rounded-l-none"
+            onClick={handleEditButton}
+          >
+            편집
+          </Button>
+        </motion.footer>
       </article>
     </Reorder.Item>
   );
--- src/components/Timeline/TimelineList.tsx
+++ src/components/Timeline/TimelineList.tsx
@@ -9,6 +9,9 @@ import {
 
 function TimelineList({ data = PolzzakItemDummyData[0] }) {
   const [cards, setCards] = useState<PolzzakCard[]>(data.itemList);
+  // 정렬된 상태 저장 시 cards 저장!
+
+  const [openCardId, setOpenCardId] = useState<number | null>(null);
 
   return (
     <Reorder.Group
@@ -20,7 +23,12 @@ function TimelineList({ data = PolzzakItemDummyData[0] }) {
       <div className="bg-gray03 absolute mx-[5px] h-full w-[1px]"></div>
       <div className="flex w-full flex-col gap-2">
         {cards.map((card) => (
-          <TimelineCard key={card.id} value={card} />
+          <TimelineCard
+            key={card.id}
+            value={card}
+            openCardId={openCardId}
+            setOpenCardId={setOpenCardId}
+          />
         ))}
       </div>
     </Reorder.Group>
