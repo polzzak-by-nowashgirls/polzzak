6f401f359a6be15fe4afaf77cc90d06adafb3298
ttining <ttining.real@gmail.com>
2025-04-03 17:24:49 +0900

    [Modify] RabbitFace 컴포넌트 수정
    - img 태그 내 size 추가

--- src/components/RabbitFace/RabbitFace.tsx
+++ src/components/RabbitFace/RabbitFace.tsx
@@ -7,8 +7,8 @@ type RabbitFaceProps = {
 
 function RabbitFace({ src, alt, size = 48, className }: RabbitFaceProps) {
   return (
-    <span className={`w-[${size}px] ${className}`} aria-hidden={true}>
-      <img src={src} alt={alt} className="h-full w-full" />
+    <span className={className} aria-hidden={true}>
+      <img src={src} alt={alt} width={size} height={size} />
     </span>
   );
 }
