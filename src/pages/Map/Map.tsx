eafad2346a4476f344f38fb3496a2eb4921fa76f
ttining <ttining.real@gmail.com>
2025-04-04 16:23:48 +0900

    [Feature] 카카오맵 생성
    - API 키 발급 (env 파일 추가)
    - 지도 생성 (`useKakaoLoader`)
    - `RootLayout` 지도 페이지 `path` 수정

--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,5 +1,28 @@
+import {
+  Map as MapArea,
+  MapMarker,
+  useKakaoLoader,
+} from 'react-kakao-maps-sdk';
+
 function Map() {
-  return <h1>지도</h1>;
+  const [loading, error] = useKakaoLoader({
+    appkey: import.meta.env.VITE_KAKAO_MAP_API_KEY,
+    libraries: ['services'], // 필요 없다면 생략 가능
+  });
+
+  if (loading) return <div>지도를 불러오는 중...</div>;
+  if (error) return <div>지도를 불러오는 데 실패했어요.</div>;
+
+  return (
+    <MapArea
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{ width: '100%', height: '100%' }}
+    >
+      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+        <div style={{ color: '#000' }}>Hello World!</div>
+      </MapMarker>
+    </MapArea>
+  );
 }
 
 export default Map;
