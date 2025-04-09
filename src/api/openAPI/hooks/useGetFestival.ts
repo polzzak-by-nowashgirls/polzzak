153e579f8627af4ca3e3d9d78bb1378cc812f262
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-04-09 23:44:54 +0900

    [Modify] props 추가 및 함수 분리
    # 141

--- src/api/openAPI/hooks/useGetFestival.ts
+++ src/api/openAPI/hooks/useGetFestival.ts
@@ -1,39 +1,91 @@
 import { useEffect, useState } from 'react';
 
 interface GetFestival {
-  areaCode: string;
+  numOfRows?: string;
+  areaCodes?: string[];
+  pageNo: string;
   eventStartDate: string;
 }
 
 type FestivalListType = Record<string, string>;
 
-function useGetFestival({ areaCode, eventStartDate }: GetFestival) {
+function useGetFestival({
+  numOfRows = '10',
+  areaCodes,
+  pageNo = '1',
+  eventStartDate,
+}: GetFestival) {
   const [festivalList, setFestivalList] = useState<FestivalListType[]>([]);
 
   useEffect(() => {
     const fetchFestivalList = async () => {
       try {
-        const res = await fetch(
-          `${import.meta.env.VITE_OPEN_API_BASE_URL}/searchFestival1?numOfRows=10&MobileApp=polzzak&areaCode=${areaCode}&MobileOS=ETC&_type=json&serviceKey=${import.meta.env.VITE_OPEN_API_KEY}&eventStartDate=${eventStartDate}`,
-        );
+        if (areaCodes && areaCodes.length > 0) {
+          const allResponse = await Promise.all(
+            areaCodes.map(async (areaCode) => {
+              const response = await fetchFestivalData(
+                numOfRows,
+                areaCode,
+                pageNo,
+                eventStartDate,
+              );
 
-        const data = await res.json();
-        const items = data?.response?.body?.items?.item;
-        const normalized = !items ? [] : Array.isArray(items) ? items : [items];
+              return response;
+            }),
+          );
 
-        setFestivalList(normalized);
+          setFestivalList(allResponse.flat());
+        } else {
+          const response = await fetchFestivalData(
+            numOfRows,
+            'none',
+            pageNo,
+            eventStartDate,
+          );
+
+          setFestivalList(response);
+        }
       } catch (error) {
         console.error(
-          `⚠️ 축제 데이터를 불러오는 중 오류 발생 (areaCode: ${areaCode}, eventStartDate: ${eventStartDate})`,
+          `⚠️ 축제 데이터를 불러오는 중 오류 발생 (areaCode: ${areaCodes}, eventStartDate: ${eventStartDate})`,
           error,
         );
       }
     };
 
     fetchFestivalList();
-  }, [areaCode, eventStartDate]);
+  }, [numOfRows, JSON.stringify(areaCodes), pageNo, eventStartDate]);
 
   return festivalList;
 }
 
 export default useGetFestival;
+
+async function fetchFestivalData(
+  numOfRows: string,
+  areaCode: string,
+  pageNo: string,
+  eventStartDate: string,
+): Promise<FestivalListType[]> {
+  const baseUrl = `${import.meta.env.VITE_OPEN_API_BASE_URL}/searchFestival1?serviceKey=${import.meta.env.VITE_OPEN_API_KEY}`;
+
+  const params = new URLSearchParams({
+    numOfRows,
+    pageNo,
+    MobileApp: 'polzzak',
+    MobileOS: 'ETC',
+    _type: 'json',
+    eventStartDate,
+  });
+
+  if (areaCode !== 'none') {
+    params.append('areaCode', areaCode);
+  }
+
+  const res = await fetch(`${baseUrl}&${params.toString()}`);
+
+  const data = await res.json();
+
+  const items = data?.response?.body?.items?.item;
+  return !items ? [] : Array.isArray(items) ? items : [items];
+}
