4b3e9d8ee9b204909e613dc58feec4b280802b28
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-04-09 15:37:47 +0900

    [Modify] 타입 변경 및 데이터 개수 추가
    # 141

--- src/api/openAPI/hooks/useGetFestival.ts
+++ src/api/openAPI/hooks/useGetFestival.ts
@@ -1,52 +1,37 @@
 import { useEffect, useState } from 'react';
 
-interface FestivalItem {
-  firstimage: string;
-  sigungucode: string;
-  cat1: string;
-  cat2: string;
-  cat3: string;
-  contentid: string;
-  booktour: string;
-  tel: string;
-  title: string;
-  addr1: string;
-  areacode: string;
-  mapy: string;
-  mlevel: string;
-  modifiedtime: string;
-  firstimage2: string;
-  mapx: string;
-  contenttypeid: string;
-  addr2: string;
-  createdtime: string;
-  cpyrhtDivCd: string;
+interface GetFestival {
+  areaCode: string;
+  eventStartDate: string;
 }
 
-function useGetFestival(): FestivalItem[] | undefined {
-  const [festivalList, setFestivalList] = useState<FestivalItem[]>();
+type FestivalListType = Record<string, string>;
+
+function useGetFestival({ areaCode, eventStartDate }: GetFestival) {
+  const [festivalList, setFestivalList] = useState<FestivalListType[]>([]);
 
   useEffect(() => {
     const fetchFestivalList = async () => {
       try {
         const res = await fetch(
-          `${import.meta.env.VITE_OPEN_API_BASE_URL}/searchFestival1?MobileApp=polzzak&MobileOS=ETC&_type=json&serviceKey=${import.meta.env.VITE_OPEN_API_KEY}&eventStartDate=20250301`,
+          `${import.meta.env.VITE_OPEN_API_BASE_URL}/searchFestival1?numOfRows=10&MobileApp=polzzak&areaCode=${areaCode}&MobileOS=ETC&_type=json&serviceKey=${import.meta.env.VITE_OPEN_API_KEY}&eventStartDate=${eventStartDate}`,
         );
 
         const data = await res.json();
         const items = data?.response?.body?.items?.item;
-        const normalized = Array.isArray(items) ? items : [items];
+        const normalized = !items ? [] : Array.isArray(items) ? items : [items];
 
         setFestivalList(normalized);
       } catch (error) {
-        console.error('⚠️ 축제 데이터를 불러오는 중 오류 발생:', error);
+        console.error(
+          `⚠️ 축제 데이터를 불러오는 중 오류 발생 (areaCode: ${areaCode}, eventStartDate: ${eventStartDate})`,
+          error,
+        );
       }
     };
 
     fetchFestivalList();
-  }, []);
-
-  if (!festivalList) return;
+  }, [areaCode, eventStartDate]);
 
   return festivalList;
 }
