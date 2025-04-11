ff581b996935960d75b5f76e4c052ef2156cfb90
ttining <ttining.real@gmail.com>
2025-04-11 15:07:36 +0900

    [Feature] 지도 - 내 위치 기능 구현
    - 내 위치 불러오기 (`navigator.geolocation` 사용)
    - 내 위치 버튼 클릭 시, 지도 중심 좌표 변경
    - 내 위치 마커 이미지 변경

--- src/components/MapHeader/MapHeader.tsx
+++ src/components/MapHeader/MapHeader.tsx
@@ -1,3 +1,4 @@
+import { MutableRefObject } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '@/components/Button/Button';
@@ -6,7 +7,17 @@ import Icon from '@/components/Icon/Icon';
 import Input from '@/components/Input/Input';
 import { useModalStore } from '@/store/useModalStore';
 
-function MapHeader() {
+type LatLng = {
+  lat: number;
+  lng: number;
+};
+
+interface MapHeaderProps {
+  mapRef: MutableRefObject<kakao.maps.Map | null>;
+  myLocation: LatLng | null;
+}
+
+function MapHeader({ mapRef, myLocation }: MapHeaderProps) {
   const navigate = useNavigate();
   const { openModal } = useModalStore();
 
@@ -16,25 +27,38 @@ function MapHeader() {
     }
     openModal('favorite');
   }
+
+  const handleClick = () => {
+    if (mapRef.current && myLocation) {
+      const center = new kakao.maps.LatLng(myLocation.lat, myLocation.lng);
+      mapRef.current.panTo(center);
+    }
+  };
+
   return (
-    <header className="fixed top-0 z-100 mb-4 flex w-full flex-col gap-3 px-4 pt-6">
-      <Input
-        label="검색"
-        hideLabel={true}
-        placeholder="검색어를 입력해 주세요."
-        className="bg-white"
-      >
-        <Button variant={'tertiary'} size="md" className="text-gray05">
-          <Icon id="search" />
-        </Button>
-      </Input>
+    <>
+      <header className="fixed top-4 right-4 left-4 z-10 flex flex-col justify-between gap-2">
+        <Input
+          label="검색"
+          hideLabel={true}
+          placeholder="검색어를 입력해 주세요."
+          className="bg-white"
+        >
+          <Button variant="tertiary" size="md" className="text-gray05">
+            <Icon id="search" />
+          </Button>
+        </Input>
+      </header>
       <Chip mode="map_filter" hideLabel={true} onClick={handleMapFilter} />
-      <div className="absolute top-full right-4">
-        <Button variant={'secondary'} size="md" className="h-11 w-11">
-          <Icon id="location" />
-        </Button>
-      </div>
-    </header>
+      <Button
+        variant="secondary"
+        size="md"
+        className="fixed top-[120px] right-4 z-10 h-10 w-10"
+        onClick={handleClick}
+      >
+        <Icon id="location" />
+      </Button>
+    </>
   );
 }
 
--- src/pages/Map/Map.tsx
+++ src/pages/Map/Map.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import {
   Map as MapArea,
   MapMarker,
@@ -9,27 +10,81 @@ import MapHeader from '@/components/MapHeader/MapHeader';
 import Modal from '@/components/Modal/Modal';
 import { useModalStore } from '@/store/useModalStore';
 
+type LatLng = {
+  lat: number;
+  lng: number;
+};
+
 function Map() {
   const [loading, error] = useKakaoLoader({
     appkey: import.meta.env.VITE_KAKAO_MAP_API_KEY,
     libraries: ['services'],
   });
+
+  // 🗺️ 지도 참조 Ref
+  const mapRef = useRef<kakao.maps.Map | null>(null);
+
+  // 🚩 내 위치 상태 저장
+  const [myLocation, setMyLocation] = useState<LatLng | null>(null);
+
   const { isOpen } = useModalStore();
 
-  if (loading) return <div>지도를 불러오는 중...</div>;
-  if (error) return <div>지도를 불러오는 데 실패했어요.</div>;
+  useEffect(() => {
+    if (!navigator.geolocation) return;
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        console.log('내 위치:', latitude, longitude);
+        setMyLocation({ lat: latitude, lng: longitude });
+      },
+      (error) => {
+        console.error('😭 위치 정보를 가져올 수 없어요. : ', error.message);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      },
+    );
+  }, []);
+
+  if (loading) return <div>🗺️ 지도를 불러오고 있어요!</div>;
+  if (error) return <div>😭 지도를 불러오는 데 실패했어요.</div>;
+  if (!myLocation) return <div>🚩 내 위치를 불러오고 있어요!</div>;
 
   return (
-    <MapArea
-      center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: '100%', height: '100%' }}
-      className="relative"
-    >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}></MapMarker>
-      <MapHeader />
-      <Outlet />
-      {isOpen && <Modal mode="slide" type="favorite" />}
-    </MapArea>
+    <>
+      <MapHeader mapRef={mapRef} myLocation={myLocation} />
+      <MapArea
+        ref={mapRef}
+        center={myLocation ?? { lat: 33.55635, lng: 126.795841 }}
+        style={{ width: '100%', height: '100%' }}
+        className="relative"
+        level={3}
+      >
+        {myLocation && (
+          <MapMarker
+            position={myLocation}
+            image={{
+              src: '/marker/my_location.svg',
+              size: {
+                width: 24,
+                height: 24,
+              },
+              options: {
+                offset: {
+                  x: 12,
+                  y: 12,
+                },
+              },
+            }}
+          ></MapMarker>
+        )}
+        <Outlet />
+        {isOpen && <Modal mode="slide" type="favorite" />}
+      </MapArea>
+    </>
   );
 }
 
