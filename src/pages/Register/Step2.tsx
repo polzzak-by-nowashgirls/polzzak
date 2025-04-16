278f9d8646049b034b7d62d911c2fd1ffa1125fd
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-16 16:05:17 +0900

    [Feature] 회원가입 기능 구현
    - 회원가입 기능 중 패스워드(Step2) 구현
    
    #147

--- src/pages/Register/Step2.tsx
+++ src/pages/Register/Step2.tsx
@@ -1,19 +1,71 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import supabase from '@/api/supabase';
 import Button from '@/components/Button/Button';
 import Input from '@/components/Input/Input';
 import Validation from '@/components/Input/Validation';
+import { validatePassword } from '@/lib/validatePassword';
 
 function Step2() {
   const navigate = useNavigate();
+  // > 비밀번호
+  const [inputPw, setInputPw] = useState('');
+  const [pwMessage, setPwMessage] = useState('');
+  const [pwValid, setPwValid] = useState<boolean | null>(null);
+  // > 비립번호 확인
+  const [checkInputPw, setCheckInputPw] = useState('');
+  const [checkPwMessage, setCheckPwMessage] = useState('');
+  const [checkPwValid, setCheckPwValid] = useState<boolean | null>(null);
 
-  const message = {
-    success: '사용 가능한 비밀번호입니다.',
-    error: '영문과 숫자, 특수문자를 포함하여 8~20자여야 합니다.',
-    errorGap: '공백은 사용할 수 없습니다.',
-    confirmSuccess: '확인되었습니다.',
-    confirmError: '입력하신 비밀번호와 일치하지 않습니다. 다시 입력해 주세요.',
-  };
+  function handleInputPw(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setInputPw(value);
+
+    const { isValid, message } = validatePassword(value);
+    setPwValid(isValid);
+    setPwMessage(isValid ? '' : message);
+  }
+  function handleCheckInputPw(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setCheckInputPw(value);
+
+    const { isValid, message } = validatePassword(value);
+    setCheckPwValid(isValid);
+    setCheckPwMessage(isValid ? '' : message);
+  }
+
+  async function handleNextButton() {
+    const PREV_USER_ID = localStorage.getItem('ex_users');
+    if (!PREV_USER_ID) {
+      console.error('해당 USER의 ID가 존재하지 않습니다.');
+      return;
+    }
+    // > supabase에서 ex_users의 step1에서 저장되었던 user_id의 row찾기
+    const { data: USER_DATA, error: fetchError } = await supabase
+      .from('ex_users')
+      .select('*')
+      .eq('user_id', PREV_USER_ID)
+      .single();
+
+    if (!USER_DATA || fetchError) {
+      console.error(fetchError, '사용자 정보를 가져오는 데 실패했습니다.');
+      return;
+    }
+
+    if (USER_DATA.password === null) {
+      const { error: UpdateError } = await supabase
+        .from('ex_users')
+        .update({ password: inputPw })
+        .eq('user_id', PREV_USER_ID);
+
+      navigate(`/register/3`);
+
+      if (UpdateError) {
+        console.error(UpdateError, '비밀번호를 저장하는 데 실패했습니다.');
+      }
+    }
+  }
 
   return (
     <>
@@ -22,21 +74,28 @@ function Step2() {
           label="비밀번호"
           type="password"
           placeholder="비밀번호를 입력해 주세요."
+          value={inputPw}
+          onChange={handleInputPw}
         />
-        <Validation status={true} message={message.success} />
+        {pwValid !== null && (
+          <Validation status={pwValid} message={pwMessage} />
+        )}
       </div>
       <div>
         <Input
           label="비밀번호 확인"
           type="password"
           placeholder="비밀번호를 한 번 더 입력해 주세요."
+          value={checkInputPw}
+          onChange={handleCheckInputPw}
         />
-        <Validation status={false} message={message.confirmError} />
+        {checkPwValid !== null && (
+          <Validation status={checkPwValid} message={checkPwMessage} />
+        )}
       </div>
       <Button
-        onClick={() => {
-          navigate('/register/3');
-        }}
+        disabled={inputPw.length === 0 || checkInputPw.length === 0}
+        onClick={handleNextButton}
       >
         다음
       </Button>
