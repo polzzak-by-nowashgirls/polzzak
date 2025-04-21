413f64384e68cdf50c4d629d7649689f781cfcd5
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-04-21 17:02:14 +0900

    [Feature] 즐겨찾기 상세 리스트 기능 구현
    - 즐겨찾기 한 리스트 불러오기
    - ListItem 컴포넌트에 data 넘기기
    
    # 158

--- src/pages/My/Favorites/FavoritesDetails.tsx
+++ src/pages/My/Favorites/FavoritesDetails.tsx
@@ -1,15 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import supabase from '@/api/supabase';
 import ListItem from '@/components/ListItem/ListItem';
+import { ListItemProps } from '@/components/ListItem/ListItem';
+import { useToast } from '@/hooks/useToast';
 import RequireLogin from '@/pages/RequireLogin';
 import { useFavoritesStore } from '@/store/useFavoritesStore';
 import { useHeaderStore } from '@/store/useHeaderStore';
 
 function FavoritesDetails() {
+  const [itemList, setItemList] = useState<ListItemProps[]>([]);
   const { id } = useParams();
   const navigate = useNavigate();
+  const showToast = useToast();
   const setContentsTitle = useHeaderStore((state) => state.setContentsTitle);
   const { folderId, folderName, setSelectFolder } = useFavoritesStore();
 
@@ -18,6 +22,7 @@ function FavoritesDetails() {
   const isAuthPrimaryId =
     localStorage.getItem('user_id') || sessionStorage.getItem('user_id');
 
+  // 🕹️ 헤더 설정
   useEffect(() => {
     if (folderId && folderName) {
       setContentsTitle(folderName);
@@ -47,13 +52,54 @@ function FavoritesDetails() {
         id: targetFolder.folder_id,
         name: targetFolder.folder_name,
       });
+
       setContentsTitle(targetFolder.folder_name);
     };
 
     checkFolderIndex();
 
     return () => setContentsTitle(null);
-  }, [id, folderId, folderName, setContentsTitle, setSelectFolder]);
+  }, [
+    id,
+    folderId,
+    folderName,
+    isAuthPrimaryId,
+    navigate,
+    setContentsTitle,
+    setSelectFolder,
+  ]);
+
+  // 🕹️ 즐겨찾기 리스트 fetch
+  useEffect(() => {
+    if (!folderId) return;
+
+    const fetchFavoriteList = async () => {
+      const { data, error } = await supabase
+        .from('ex_favorite')
+        .select(
+          'ex_contents (contentid, firstimage, title, eventstartdate, eventenddate, region, district, likes, reviews)',
+        )
+        .eq('folder_id', folderId);
+
+      if (error) {
+        showToast(
+          '데이터를 불러오지 못했어요. 잠시 후 다시 시도해 주세요.',
+          'top-[64px]',
+          5000,
+        );
+        console.error('❌ 데이터 가져오기 실패: ', error);
+        return;
+      }
+
+      const myContents = (data ?? [])
+        .flatMap((item) => item.ex_contents || [])
+        .filter(Boolean);
+
+      setItemList(myContents);
+    };
+
+    fetchFavoriteList();
+  }, [folderId, showToast, setItemList]);
 
   if (!isAuth || !isAuthPrimaryId) {
     return <RequireLogin />;
@@ -61,7 +107,7 @@ function FavoritesDetails() {
 
   return (
     <main className="p-6">
-      <ListItem />
+      <ListItem data={itemList} />
     </main>
   );
 }
