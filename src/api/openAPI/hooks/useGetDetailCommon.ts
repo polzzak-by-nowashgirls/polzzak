937c69574a5a4a718b166697224e0d34692a028e
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-04-28 01:38:33 +0900

    [Rename] 오타난 파일명 변경 user -> use # 167

--- src/api/openAPI/hooks/useGetDetailCommon.ts
+++ src/api/openAPI/hooks/useGetDetailCommon.ts
@@ -1,16 +1,34 @@
 import { useEffect, useState } from 'react';
 
-import { getLikesAndReviews } from '@/api/supabase/utils';
-
-export type detailsTypes = {
-  [key: string]: string;
-} & {
-  likes: number;
-  reviews: number;
-};
+interface detailsTypes {
+  overview: string;
+  contentid: string;
+  sigungucode: string;
+  cat1: string;
+  cat2: string;
+  cat3: string;
+  addr1: string;
+  addr2: string;
+  zipcode: string;
+  mapx: string;
+  mapy: string;
+  mlevel: string;
+  cpyrhtDivCd: string;
+  contenttypeid: string;
+  booktour: string;
+  createdtime: string;
+  homepage: string;
+  modifiedtime: string;
+  tel: string;
+  telname: string;
+  title: string;
+  firstimage: string;
+  firstimage2: string;
+  areacode: string;
+}
 
 function useGetDetailCommon(contentId: string) {
-  const [details, setDetails] = useState<detailsTypes | null>(null);
+  const [details, setDetails] = useState<detailsTypes[]>([]);
 
   useEffect(() => {
     const fetchDetailCommon = async () => {
@@ -18,24 +36,10 @@ function useGetDetailCommon(contentId: string) {
         const res = await fetch(
           `/tourapi/${import.meta.env.VITE_OPEN_API_BASE_URL}/detailCommon1?serviceKey=${import.meta.env.VITE_OPEN_API_KEY}&MobileApp=polzzak&MobileOS=ETC&_type=json&pageNo=1&numOfRows=20&contentId=${contentId}&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&catcodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&&overviewYN=Y`,
         );
-        const commonData = await res.json();
-        const item = commonData?.response?.body?.items?.item?.[0];
+        const data = await res.json();
+        const items = data?.response?.body?.items?.item ?? [];
 
-        if (!item) return;
-
-        const resIntro = await fetch(
-          `/tourapi/${import.meta.env.VITE_OPEN_API_BASE_URL}/detailIntro1?serviceKey=${import.meta.env.VITE_OPEN_API_KEY}&MobileApp=polzzak&MobileOS=ETC&_type=json&pageNo=1&numOfRows=20&contentId=${contentId}&contentTypeId=${item?.contenttypeid}&introYN=Y`,
-        );
-        const introData = await resIntro.json();
-        const intro = introData?.response?.body?.items?.item?.[0];
-
-        const stats = await getLikesAndReviews(contentId);
-
-        setDetails({
-          ...item,
-          ...intro,
-          ...stats,
-        });
+        setDetails(items);
       } catch (err) {
         console.error(err);
       }
