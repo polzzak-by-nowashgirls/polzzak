0bdb971dc04801f7dc80d0147be96622c18dd1b7
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-01 16:27:09 +0900

    [Feature] 폴짝 추가 페이지 기능 구현
    - 폴짝명 20자 이내로 작성
    - 날짜 선택 및 초기화
    - 지역 여러 개 선택
    - 사진 선택 및 자르기 + 미리보기
    - 사진 삭제
    - 폴짝명, 날짜, 지역 중 하나라도 값 있을 때만 저장 버튼 활성화
    - 버튼 클릭 시, 상세 페이지로 이동
    
    # 174

--- src/pages/Polzzak/Add/Add.tsx
+++ src/pages/Polzzak/Add/Add.tsx
@@ -1,54 +1,215 @@
+import { format } from 'date-fns';
+import { ko } from 'date-fns/locale';
+import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import Button from '@/components/Button/Button';
+import CalendarCustom from '@/components/Calendar/CalendarCustom';
 import Chip from '@/components/Chip/Chip';
+import SlideUpDialog from '@/components/Dialog/SlideUpDialog';
 import Icon from '@/components/Icon/Icon';
+import { ImageCropper } from '@/components/Input/ImageCropper';
 import Input from '@/components/Input/Input';
-import Validation from '@/components/Input/Validation';
-import Modal from '@/components/Modal/Modal';
-import { useModalStore } from '@/store/useModalStore';
+import { useDialogStore } from '@/store/useDialogStore';
+import { usePolzzakStore } from '@/store/usePolzzakStroe';
 
 function Add() {
-  const { openModal } = useModalStore();
+  const navigate = useNavigate();
+  const [showCropper, setShowCropper] = useState(false);
+  const { isOpen, openModal, closeModal } = useDialogStore();
+  const {
+    name,
+    thumbnail,
+    dateRange,
+    region,
+    imageUrl,
+    setName,
+    setDateRange,
+    setRegion,
+    setThumbnail,
+    setFileName,
+    setImageUrl,
+    reset,
+  } = usePolzzakStore();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    reset();
+  }, [reset]);
+
+  const handleCalender = () => {
+    if (isOpen) {
+      closeModal();
+    } else {
+      openModal();
+    }
+  };
+
+  const calenderBtn = [
+    {
+      text: dateRange?.from
+        ? dateRange?.to
+          ? `${format(dateRange.from, 'yyyy-MM-dd(eee)', { locale: ko })} ~ ${format(dateRange.to, 'yyyy-MM-dd(eee)', { locale: ko })}`
+          : `${format(dateRange.from, 'yyyy-MM-dd(eee)', { locale: ko })}`
+        : '폴짝 타이밍을 선택해 주세요.',
+
+      onClick: () => {
+        closeModal();
+      },
+    },
+    {
+      text: '초기화',
+      onClick: () => {
+        setDateRange(undefined);
+        closeModal();
+      },
+    },
+  ];
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    const url = URL.createObjectURL(file);
+    setImageUrl(url);
+    setFileName(file?.name);
+  };
+
+  const handleCropDone = (blob: Blob) => {
+    const previewUrl = URL.createObjectURL(blob);
+    setThumbnail(previewUrl);
+    setShowCropper(false);
+  };
 
   return (
-    <section className="flex h-full w-full flex-col justify-between">
+    <section className="flex flex-col justify-between gap-10">
       <h1 className="sr-only">폴짝 추가하기</h1>
       <div className="flex flex-1 flex-col gap-4">
         <div>
           <Input
             label="폴짝 이름"
             type="text"
-            placeholder="폴짝 이름을 입력해 주세요."
+            placeholder={name ? name : '폴짝 이름을 입력해 주세요.'}
+            value={name ?? ''}
+            onChange={(e) => setName(e.target.value)}
+            maxLength={20}
           />
         </div>
         <div>
           <Input
             label="폴짝 날짜"
-            type="text"
-            placeholder="폴짝 타이밍을 입력해 주세요."
+            type="button"
+            value={
+              dateRange?.from
+                ? dateRange?.to
+                  ? `${format(dateRange.from, 'yyyy-MM-dd', { locale: ko })} ~ ${format(dateRange.to, 'yyyy-MM-dd', { locale: ko })}`
+                  : `${format(dateRange.from, 'yyyy-MM-dd', { locale: ko })}`
+                : '폴짝 타이밍을 선택해 주세요.'
+            }
+            onClick={handleCalender}
+            className={dateRange?.from && 'text-black'}
           >
-            <Button variant={'tertiary'} size="md" onClick={openModal}>
+            <Button variant={'tertiary'} size="md" onClick={handleCalender}>
               <Icon id="calendar" className="text-gray05" />
             </Button>
           </Input>
-          <Validation status={false} message="필수 입력 항목입니다." />
         </div>
         <div>
-          <Chip mode="region" />
+          <Chip
+            mode="region"
+            label="지역 선택"
+            subLabel="다중 선택 가능합니다."
+            type="multiple"
+            onClick={(selectChip) => {
+              console.log(selectChip);
+              setRegion((prev) => {
+                if (!prev) return [selectChip.name];
+
+                const isAlreadySelected = prev.includes(selectChip.name);
+                return isAlreadySelected
+                  ? prev.filter((chip) => chip !== selectChip.name)
+                  : [...prev, selectChip.name];
+              });
+            }}
+          />
         </div>
         <div>
           <Input
             label="폴짝 사진"
             type="file"
             placeholder="폴짝 사진을 등록해 주세요."
+            onClick={() => setShowCropper(true)}
+            onChange={handleFileChange}
+            ref={inputRef}
           >
-            <Button variant={'tertiary'} size="md">
+            <Button
+              variant={'tertiary'}
+              size="md"
+              onClick={() => {
+                inputRef.current?.click();
+              }}
+            >
               <Icon id="image" className="text-gray05" />
             </Button>
           </Input>
         </div>
+
+        {thumbnail && (
+          <figure>
+            <figcaption className="fs-14 m-1 px-1">
+              폴짝 사진 미리보기
+            </figcaption>
+            <div className="relative w-1/2">
+              <img
+                src={thumbnail}
+                alt="선택한 폴짝 사진 미리보기"
+                className="rounded-lg border drop-shadow-md"
+              />
+              <Button
+                className="absolute top-2 right-2 h-6 w-6"
+                variant={'float'}
+                size={'md'}
+                onClick={() => {
+                  setThumbnail(null);
+                  setFileName(null);
+                  setImageUrl(null);
+                }}
+              >
+                <Icon id="close" />
+              </Button>
+            </div>
+          </figure>
+        )}
       </div>
-      <Button>폴짝 추가하기</Button>
-      <Modal mode="slide" type="calendar" />
+      <Button
+        disabled={
+          !name && !dateRange?.from && (!region || region?.length === 0)
+        }
+        onClick={() => {
+          // 서버 저장할 동안 로더 띄우기
+          reset();
+          navigate('/polzzak/1', { replace: true });
+          console.log('서버에 저장 완료!');
+        }}
+      >
+        폴짝 추가하기
+      </Button>
+      {isOpen && (
+        <SlideUpDialog
+          header="폴짝 날짜 선택"
+          button={calenderBtn}
+          buttonDirection="col"
+        >
+          <CalendarCustom
+            selected={dateRange ?? undefined}
+            onSelect={setDateRange}
+          />
+        </SlideUpDialog>
+      )}
+      {showCropper && imageUrl && (
+        <ImageCropper imageUrl={imageUrl} onCropDone={handleCropDone} />
+      )}
     </section>
   );
 }
---
+++ src/store/usePolzzakStroe.ts
@@ -0,0 +1,45 @@
+import { DateRange } from 'react-day-picker';
+import { create } from 'zustand';
+
+interface PolzzakStore {
+  name: string | null;
+  dateRange: DateRange | null;
+  region: string[] | null;
+  thumbnail: string | null;
+  fileName: string | null;
+  imageUrl: string | null;
+
+  setName: (name: string) => void;
+  setDateRange: (date: DateRange | undefined) => void;
+  setRegion: (regionUpdate: (prev: string[] | null) => string[]) => void;
+  setThumbnail: (thumbnail: string | null) => void;
+  setFileName: (fileName: string | null) => void;
+  setImageUrl: (imageUrl: string | null) => void;
+  reset: () => void;
+}
+
+export const usePolzzakStore = create<PolzzakStore>()((set) => ({
+  name: null,
+  dateRange: null,
+  region: null,
+  thumbnail: null,
+  fileName: null,
+  imageUrl: null,
+
+  setName: (name) => set({ name }),
+  setDateRange: (date) => set({ dateRange: date ?? null }),
+  setRegion: (regionUpdate) =>
+    set((state) => ({ region: regionUpdate(state.region) })),
+  setThumbnail: (thumbnail) => set({ thumbnail }),
+  setFileName: (fileName) => set({ fileName }),
+  setImageUrl: (imageUrl) => set({ imageUrl }),
+  reset: () =>
+    set({
+      name: null,
+      dateRange: null,
+      region: null,
+      thumbnail: null,
+      fileName: null,
+      imageUrl: null,
+    }),
+}));
