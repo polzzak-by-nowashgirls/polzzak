84991098f26640f72ac476a7aaa9661b69c6477f
kimHyeonJu <kimhyesom1202@gmail.com>
2025-05-09 16:45:28 +0900

    [Feature] ListItem 컴포넌트
    - ListItemProps 추가
    - ListItem 클릭 시 url 변경
    - getAddress에서 address가 없을 경우 추가
    - key값 추가
    
    #170

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -9,19 +9,20 @@ import { useSearchStore } from '@/store/useSearchStore';
 
 export interface ListItemProps {
   contentid: string;
-  contenttypeid?: string;
+  contenttypeid: string;
   title: string;
   addr1: string;
   image: string;
-  eventstartdate?: string | '';
-  eventenddate?: string | '';
-  opentimefood?: string;
-  usetime?: string;
-  usetimeleports?: string;
-  usetimeculture?: string;
-  opentime?: string;
-  checkintime?: string;
-  checkouttime?: string;
+  eventstartdate: string;
+  eventenddate: string;
+  opentimefood: string;
+  usetime: string;
+  usetimeleports: string;
+  usetimeculture: string;
+  opentime: string;
+  checkintime: string;
+  checkouttime: string;
+  restdate: string;
   reviews: number;
   likes: number;
 }
@@ -34,11 +35,11 @@ function ListItem({ data }: ListItemData) {
   const navigate = useNavigate();
   const { detailData, setDetailData } = useSearchStore();
   const handleFavorite = (contentid: string) => {
-    console.log(`즐겨찾기 저장! ${contentid}`);
+    console.log(`즐겨찾기 저장!, ${contentid}`);
   };
-  const getAddress = (address: string) => {
-    const [city = '', province = ''] = address.split(' ');
-
+  const getAddress = (address: string = '') => {
+    const addressStr = address || '';
+    const [city = '', province = ''] = addressStr.split(' ');
     return { city, province };
   };
   useEffect(() => {
@@ -60,7 +61,7 @@ function ListItem({ data }: ListItemData) {
     };
 
     fetchDetails();
-  }, [data]);
+  }, [data, setDetailData]);
 
   const changeDate = (date: string) => {
     const yy = date.slice(0, 4);
@@ -68,117 +69,124 @@ function ListItem({ data }: ListItemData) {
     const dd = date.slice(6, 8);
     return `${yy}.${mm}.${dd}`;
   };
-  const handleMoveDetail = (contentid: string) => {
-    navigate(`/contents/${contentid}`);
+  const handleMoveDetail = (id: string) => {
+    navigate(`/contents/${id}`);
   };
+  console.log(detailData);
 
   return (
     <ul className="flex flex-col gap-6">
-      {detailData.map((item) => {
-        const { city, province } = getAddress(item.addr1);
+      {detailData.map((item, index) => {
+        const { city, province } = getAddress(item?.addr1);
         const time = [
           item.opentimefood,
           item.usetime,
           item.usetimeleports,
           item.usetimeculture,
           item.opentime,
+          item.restdate,
         ];
 
         return (
           <li
-            key={item.contentid}
-            className="flex-start relative flex w-full items-center gap-4"
+            key={`${item.contentid}-${index}`}
             onClick={() => handleMoveDetail(item.contentid)}
           >
-            <div className="bg-primary/10 flex h-20 w-20 shrink-0 items-center justify-center overflow-hidden rounded-2xl object-cover">
-              {item.image === '' ? (
-                <RabbitFace alt="이미지 준비 중입니다." />
-              ) : (
-                <img
-                  src={item.image}
-                  alt={item.title}
-                  className="h-full w-full"
-                />
-              )}
-            </div>
-            <div className="flex w-0 flex-1 flex-col">
-              <div className="flex items-center justify-between">
-                <h3
-                  className="fs-14 ls lh w-full truncate font-semibold text-black"
-                  aria-label={item.title}
-                >
-                  {item.title}
-                </h3>
-                <Button
-                  variant="tertiary"
-                  size="md"
-                  className='m-0.5 h-6 w-6 [&_svg:not([class*="size-"])]:size-5'
-                  onClick={(e) => {
-                    e.preventDefault();
-                    handleFavorite(item.contentid);
-                  }}
-                  // aria-label={isCheck ? '즐겨찾기 취소' : '즐겨찾기 추가'}
-                  aria-live="polite"
-                >
-                  <Icon
-                    id={
-                      // isCheck[item.contentid] ? 'favorite_on' : 'favorite_off'
-                      !item.contentid ? 'favorite_on' : 'favorite_off'
-                    }
-                    className="text-primary"
+            <div className="flex-start relative flex w-full items-center gap-4">
+              <div className="bg-primary/10 flex h-20 w-20 shrink-0 items-center justify-center overflow-hidden rounded-2xl object-cover">
+                {item.image === '' ? (
+                  <RabbitFace alt="이미지 준비 중입니다." />
+                ) : (
+                  <img
+                    src={item.image}
+                    alt={item.title}
+                    className="h-full w-full"
                   />
-                </Button>
+                )}
               </div>
-              <>
-                {time.map(
-                  (text, index) =>
-                    text && (
-                      <span
-                        className="fs-14 ls lh font-regular text-gray07"
-                        key={index}
-                      >
-                        {text}
-                      </span>
-                    ),
-                )}
-                {item.eventstartdate && (
-                  <span className="fs-14 ls lh font-regular text-gray07">
-                    {changeDate(item?.eventstartdate)} &#126;{' '}
-                    {changeDate(item?.eventenddate)}
+              <div className="flex w-0 flex-1 flex-col">
+                <div className="flex items-center justify-between">
+                  <h3
+                    className="fs-14 ls lh w-full truncate font-semibold text-black"
+                    aria-label={item.title}
+                  >
+                    {item.title}
+                  </h3>
+                  <Button
+                    variant="tertiary"
+                    size="md"
+                    className='m-0.5 h-6 w-6 [&_svg:not([class*="size-"])]:size-5'
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleFavorite(item.contentid);
+                    }}
+                    // aria-label={isCheck ? '즐겨찾기 취소' : '즐겨찾기 추가'}
+                    aria-live="polite"
+                  >
+                    <Icon
+                      id={
+                        // isCheck[item.contentid] ? 'favorite_on' : 'favorite_off'
+                        !item.contentid ? 'favorite_on' : 'favorite_off'
+                      }
+                      className="text-primary"
+                    />
+                  </Button>
+                </div>
+                <>
+                  {time.map(
+                    (text, index) =>
+                      text && (
+                        <span
+                          className="fs-14 ls lh font-regular text-gray07"
+                          key={index}
+                        >
+                          {text}
+                        </span>
+                      ),
+                  )}
+                  {item.eventstartdate && (
+                    <span className="fs-14 ls lh font-regular text-gray07">
+                      {changeDate(item?.eventstartdate)} &#126;{' '}
+                      {changeDate(item?.eventenddate)}
+                    </span>
+                  )}
+                  {item.checkintime && (
+                    <span className="fs-14 ls lh font-regular text-gray07">
+                      {`체크인 : ${item?.checkintime}, 체크아웃 : ${item?.checkouttime}`}
+                    </span>
+                  )}
+                </>
+                {item.addr1 && (
+                  <span className="fs-14 ls lh font-regular text-gray07 inline-flex items-center">
+                    {city}
+                    <Icon
+                      id="chevron_right"
+                      size={16}
+                      className="text-gray07"
+                    />
+                    {province}
                   </span>
                 )}
-                {item.checkintime && (
-                  <span className="fs-14 ls lh font-regular text-gray07">
-                    {`체크인 : ${item?.checkintime}, 체크아웃 : ${item?.checkouttime}`}
-                  </span>
-                )}
-              </>
-              {item.addr1 && (
-                <span className="fs-14 ls lh font-regular text-gray07 inline-flex items-center">
-                  {city}
-                  <Icon id="chevron_right" size={16} className="text-gray07" />
-                  {province}
-                </span>
-              )}
-              <dl className="fs-14 ls lh font-regular text-gray07 flex items-center gap-2">
-                <div
-                  className="flex items-center gap-1"
-                  aria-label="즐겨찾기 수"
-                >
-                  <dt>
-                    <Icon id="favorite_off" size={16} />
-                  </dt>
-                  <dd className="align-top">
-                    {item.likes >= 999 ? '999+' : item.likes}
-                  </dd>
-                </div>
-                <div className="flex items-center gap-1" aria-label="리뷰 수">
-                  <dt>
-                    <Icon id="review" size={16} />
-                  </dt>
-                  <dd>{item.reviews >= 999 ? '999+' : item.reviews}</dd>
-                </div>
-              </dl>
+                <dl className="fs-14 ls lh font-regular text-gray07 flex items-center gap-2">
+                  <div
+                    className="flex items-center gap-1"
+                    aria-label="즐겨찾기 수"
+                  >
+                    <dt>
+                      <Icon id="favorite_off" size={16} />
+                    </dt>
+                    <dd className="align-top">
+                      {item.likes >= 999 ? '999+' : item.likes}
+                    </dd>
+                  </div>
+                  <div className="flex items-center gap-1" aria-label="리뷰 수">
+                    <dt>
+                      <Icon id="review" size={16} />
+                    </dt>
+                    <dd>{item.reviews >= 999 ? '999+' : item.reviews}</dd>
+                  </div>
+                </dl>
+              </div>
             </div>
           </li>
         );
