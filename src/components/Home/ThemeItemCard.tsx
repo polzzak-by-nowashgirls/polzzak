89d28626a55c8d66e00ab0179c8764221e3b985b
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-21 17:54:42 +0900

    [Modify] 타입 속성명 수정 및 이미지 없을 때 대체 이미지 로직 추가
    # 219

--- src/components/Home/ThemeItemCard.tsx
+++ src/components/Home/ThemeItemCard.tsx
@@ -2,29 +2,37 @@ import { Link } from 'react-router-dom';
 
 import { CarouselItem } from '@/components/Home/Carousel';
 
+import RabbitFace from '../RabbitFace/RabbitFace';
+
 export interface ThemeItem {
-  contentId: string;
-  img: string;
+  contentid: string;
   title: string;
-  region: string;
+  addr1: string;
+  firstimage: string;
 }
 
 function ThemeItemCard({ item }: { item: ThemeItem }) {
-  const { contentId, img, title, region } = item;
+  const { contentid, firstimage, title, addr1 } = item;
 
   return (
     <CarouselItem className="shrink-0 basis-[150px]">
       {/* 상세페이지 생성 후 수정 */}
-      <Link to={`/contents/${contentId}`}>
+      <Link to={`/contents/${contentid}`}>
         <figure>
-          <img
-            src={img}
-            alt={title}
-            className="aspect-[4/3] w-full rounded-md object-cover object-center"
-          />
+          <div className="bg-primary/10 flex aspect-[4/3] w-full items-center justify-center rounded-md">
+            {firstimage ? (
+              <img
+                src={firstimage}
+                alt={title}
+                className="aspect-[4/3] w-full rounded-md object-cover object-center"
+              />
+            ) : (
+              <RabbitFace alt="이미지 준비 중입니다." size={55} />
+            )}
+          </div>
           <figcaption className="mt-2 px-1">
             <p className="fs-14 lh truncate font-medium">{title}</p>
-            <p className="fs-13 text-gray06 lh truncate">{region}</p>
+            <p className="fs-13 text-gray06 lh truncate">{addr1}</p>
           </figcaption>
         </figure>
       </Link>
