82751651cc93ccfd7519ad78d11949dbe7f2870f
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-05-21 17:47:55 +0900

    [Modify] fetchGetFestival로 함수 이동
    # 219

--- src/api/openAPI/index.ts
+++ src/api/openAPI/index.ts
@@ -1,3 +1,2 @@
 export { useGetAreaCode } from '@/api/openAPI/hooks/useGetAreaCode';
 export { useGetDetailCommon } from '@/api/openAPI/hooks/useGetDetailCommon';
-export { useGetDetailCommons } from '@/api/openAPI/hooks/useGetDetailCommon';
